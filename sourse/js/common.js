2fb78d906564f46994bce7b8d0c853aa4d9f8eab
MaxSimakov <Simakov.maxim82@gmail.com>
2021-12-09 18:01:00 +0300

    edit sSexEducChapter

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -304,28 +304,11 @@ function eventHandler() {
 		slidesPerView: 'auto', 
 		watchOverflow: true, 
 		spaceBetween: 0,
+		// loop: true,
 		navigation: {
 			nextEl: '.swiper-button-next',
 			prevEl: '.swiper-button-prev',
-		},
-		breakpoints: {
-			// when window width is >= 320px
-			// 576: {
-			// 	slidesPerView: 2,
-			// 	spaceBetween: 16
-			// },
-			// // when window width is >= 480px
-			// 768: {
-			// 	slidesPerView: 3,
-			// 	spaceBetween: 16
-			// },
-			// when window width is >= 640px
-			// 640: {
-			// 	slidesPerView: 4,
-			// 	spaceBetween: 40
-			// }
 		}
-
 	});
 
 
